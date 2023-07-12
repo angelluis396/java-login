04de6f056a2d1985a99497d7cfd9e5a922555953
aliceaa396 <ala31996@gmail.com>
2023-07-12 17:01:18 -0400

    created GUI Version of initial project

---
+++ src/main/java/GUI.java
@@ -0,0 +1,34 @@
+import javax.swing.*;
+
+public class GUI {
+    public static void main(String[] args) {
+
+        JPanel panel = new JPanel();
+
+        JFrame frame = new JFrame();
+        frame.setSize(100, 100);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.add(panel);
+
+        panel.setLayout(null);
+
+        JLabel userLabel = new JLabel("User");
+        userLabel.setBounds(10, 20, 80, 25);
+        panel.add(userLabel);
+
+        JTextField userText = new JTextField(20);
+        userText.setBounds(100, 20, 165, 25);
+        panel.add(userText);
+
+        JLabel passwordLabel = new JLabel("Password");
+        passwordLabel.setBounds(10, 50, 80, 25);
+        panel.add(passwordLabel);
+
+        JPasswordField passwordText = new JPasswordField();
+        passwordText.setBounds(100, 50, 165, 25);
+        panel.add(passwordText);
+
+        frame.setVisible(true);
+
+    }
+}
