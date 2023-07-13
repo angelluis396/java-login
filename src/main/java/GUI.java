3c61bdd7b163a1959407669f97c132b0139ff6f8
aliceaa396 <ala31996@gmail.com>
2023-07-13 09:14:12 -0400

    completed GUI version 1 of login app, without signup or database integration.

--- src/main/java/GUI.java
+++ src/main/java/GUI.java
@@ -1,6 +1,14 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class GUI {
+public class GUI implements ActionListener {
+    private static JLabel userLabel;
+    private static JTextField userText;
+    private static JLabel passwordLabel;
+    private static JPasswordField passwordText;
+    private static JButton button;
+    private static JLabel messageLabel;
     public static void main(String[] args) {
 
         JPanel panel = new JPanel();
@@ -12,23 +20,46 @@ public class GUI {
 
         panel.setLayout(null);
 
-        JLabel userLabel = new JLabel("User");
+        userLabel = new JLabel("User");
         userLabel.setBounds(10, 20, 80, 25);
         panel.add(userLabel);
 
-        JTextField userText = new JTextField(20);
+        userText = new JTextField(20);
         userText.setBounds(100, 20, 165, 25);
         panel.add(userText);
 
-        JLabel passwordLabel = new JLabel("Password");
+        passwordLabel = new JLabel("Password");
         passwordLabel.setBounds(10, 50, 80, 25);
         panel.add(passwordLabel);
 
-        JPasswordField passwordText = new JPasswordField();
+        passwordText = new JPasswordField();
         passwordText.setBounds(100, 50, 165, 25);
         panel.add(passwordText);
 
+        button = new JButton("Login");
+        button.setBounds(10,80,80,25);
+        button.addActionListener(new GUI());
+        panel.add(button);
+
+        messageLabel = new JLabel("");
+        messageLabel.setBounds(10,110,300,25);
+        panel.add(messageLabel);
+
         frame.setVisible(true);
 
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        String user = userText.getText();
+        String password = passwordText.getText();
+
+        System.out.println(user + "," + password);
+
+        if ( user.isBlank() || password.isBlank() ){
+            messageLabel.setText("User name or Password Cant Be Blank!");
+        } else if (user.equals("user") && (password.equals("password"))){
+            messageLabel.setText("Successful Login!");
+        }
     }
 }
